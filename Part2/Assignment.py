39cc978437b44c7a2dd9c066c2334a235cf77796
Archkral <k.rado@hotmail.com>
2020-04-08 13:58:48 +0200

    Update Assignment.py

--- Part2/Assignment.py
+++ Part2/Assignment.py
@@ -20,6 +20,7 @@ u_k = np.array(mat['U_k'])  # 3x10001 input array - U_k = np.array([u_dot, v_dot
 
 # Q2.3
 Ex_00 = np.array([[150, 0, 40, 0.4]]).T  # optimal state estimate
+# Nominal state estimate (mean/stdev)
 alpha = sum(z_k[:, 0]) / len(z_k)
 astd = sqrt(sum((z_k[:, 0]-alpha)**2) / (len(z_k)-1))
 beta = sum(z_k[:, 1]) / len(z_k)
@@ -41,12 +42,12 @@ B = np.array([[1, 0, 0],
               [0, 0, 0]]).astype(float)
 G = np.diag([1, 1, 1, 0]).astype(float)
 
-# Setup
+# Setting up variable names for iteration
 x = x_00
 x_k1k1 = Ex_00
 P_k1k1 = P_00
 
-# Iterator
+# Time/errors/loop variables and data storage variables
 ti = 0
 dt = 0.01
 tf = dt
@@ -68,6 +69,7 @@ rankHF = kf_obs(dHx, dFx)
 if rankHF < len(x):
     print('The current state is not observable; rank of Observability Matrix is (', rankHF,'), but it should be (', len(x), ').\n')
 
+# I don't get why the F jacobian can't be taken out of the loop, cause it doesn't depend on u_k by definition?
 for k in range(N):
 
     uk = np.array([u_k[k]]).T
@@ -90,7 +92,7 @@ for k in range(N):
         its += 1
         err = 0
 
-        dHx = kf_dhx(tf, eta1, uk)
+        dHx = kf_dhx(tf, eta1, uk)  # output jacobian
         K = P_kk1 @ dHx.T @ np.linalg.inv(dHx @ P_kk1 @ dHx.T + R)
         z_k1k1 = kf_hx(tf, eta1, uk)  # predicted output
         x_k1k1 = x_kk1 + K @ (z_kk1 - z_k1k1 - dHx@(x_kk1 - eta1))
@@ -114,131 +116,3 @@ for k in range(N):
     for i in range(len(x_k1k1)):
         stdx_corr[k, i] = P_k1k1[i, i]
 
-"""
-"""
-"""
-    XX_k1k1(:,k) = x_k_1k_1;
-%     PP_k1k1(k,:) = P_k_1k_1;
-    STDx_cor(:,k) = stdx_cor;
-end
-
-% calculate state estimation error (in real life this is unknown!)
-EstErr = (XX_k1k1-X_k);
-
-fprintf('IEKF state estimation error RMS = %d, completed run with %d samples in %2.2f seconds.\n', sqrt(mse(EstErr)), N, time2);
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Plotting
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-close all;
-
-plotID = 1000;
-figure(plotID);
-set(plotID, 'Position', [1 550 600 400], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-hold on;
-plot(X_k(1,:), 'b');
-plot(X_k(2,:), 'b--');
-plot(Z_k(1,:), 'k');
-% plot(Z_k(2,:), 'k--');
-title('True state (blue) and Measured state (black)');
-if (printfigs == 1)
-    fpath = sprintf('fig_demoKFStateMeasurement');
-    savefname = strcat(figpath, fpath);
-    print(plotID, '-dpng', '-r300', savefname);
-end
-
-
-plotID = 1001;
-figure(plotID);
-set(plotID, 'Position', [1 100 600 400], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-hold on;
-plot(X_k(1,:), 'b');
-plot(X_k(2,:), 'b--');
-plot(XX_k1k1(1,:), 'r');
-plot(XX_k1k1(2,:), 'r--');
-title('True state (blue) and Estimated state (red)');
-if (printfigs == 1)
-    fpath = sprintf('fig_demoKFStateEstimates');
-    savefname = strcat(figpath, fpath);
-    print(plotID, '-dpng', '-r300', savefname);
-end
-
-
-
-plotID = 2001;
-figure(plotID);
-set(plotID, 'Position', [800 100 600 600], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-subplot(2, 1, 1);
-plot(EstErr(1,:), 'b');
-title('State 1 estimation error');
-subplot(2, 1, 2);
-plot(EstErr(2,:), 'b');
-title('State 2 estimation error');
-if (printfigs == 1)
-    fpath = sprintf('fig_demoKFStatesEstimateErrors');
-    savefname = strcat(figpath, fpath);
-    print(plotID, '-dpng', '-r300', savefname);
-end
-
-plotID = 2002;
-figure(plotID);
-set(plotID, 'Position', [800 550 600 400], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-subplot(2, 1, 1);
-plot(EstErr(1,:), 'b');
-axis([0 50 min(EstErr(1,:)) max(EstErr(1,:))]);
-title('State 1 estimation error (Zoomed in)');
-subplot(2, 1, 2);
-plot(EstErr(2,:), 'b');
-axis([0 50 min(EstErr(2,:)) max(EstErr(2,:))]);
-title('State 2 estimation error (Zoomed in)');
-if (printfigs == 1)
-    fpath = sprintf('fig_demoKFStatesEstimateErrorsZoom');
-    savefname = strcat(figpath, fpath);
-    print(plotID, '-dpng', '-r300', savefname);
-end
-
-
-% plotID = 2003;
-% figure(plotID);
-% set(plotID, 'Position', [1000 100 600 400], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-% hold on;
-% plot(EstErr, 'b');
-% plot(STDx_cor, 'r');
-% plot(-STDx_cor, 'g');
-% legend('Estimation error', 'Upper error STD', 'Lower error STD', 'Location', 'northeast');
-% title('State estimation error with STD of Innovation');
-% if (printfigs == 1)
-%     fpath = sprintf('fig_demoKFStatesEstimatesMeasurements');
-%     savefname = strcat(figpath, fpath);
-%     print(plotID, '-dpng', '-r300', savefname);
-% end
-%
-%
-% plotID = 2004;
-% figure(plotID);
-% set(plotID, 'Position', [1000 550 600 400], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-% hold on;
-% plot(EstErr, 'b');
-% plot(STDx_cor, 'r');
-% plot(-STDx_cor, 'g');
-% axis([0 50 min(EstErr) max(EstErr)]);
-% title('State estimation error');
-% legend('Estimation error', 'Upper error STD', 'Lower error STD', 'Location', 'northeast');
-% if (printfigs == 1)
-%     fpath = sprintf('fig_demoKFStatesEstimatesMeasurements');
-%     savefname = strcat(figpath, fpath);
-%     print(plotID, '-dpng', '-r300', savefname);
-% end
-
-plotID = 3001;
-figure(plotID);
-set(plotID, 'Position', [1 700 600 300], 'defaultaxesfontsize', 10, 'defaulttextfontsize', 10, 'PaperPositionMode', 'auto');
-hold on;
-plot(IEKFitcount, 'b');
-title('IEKF iterations at each sample');
-if (printfigs == 1)
-    fpath = sprintf('fig_demoKFStatesEstimatesMeasurements');
-    savefname = strcat(figpath, fpath);
-    print(plotID, '-dpng', '-r300', savefname);
-end
-    """
